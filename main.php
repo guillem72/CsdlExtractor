213066130f14ac6f0aeb9127879485ce0b01d7d7
Guillem Lluch Moll <guillem72@gmail.com>
2016-12-17 15:38:26 +0100

    Working on multiple files.

--- main.php
+++ main.php
@@ -4,12 +4,46 @@
  * Date: 11/12/16
  * Time: 02:18
  */
-include('CsdlExtractor.class.php');
-$csdl = new \glluchcom\csdlScraping\Extractor("information services");
+// All files in a dir ls -l | grep .html | awk '{print $9, $10, $11 }'
 
-$titles = $csdl->getTitles(); //extract
-echo "Titles: " . PHP_EOL;
-foreach ($titles as $title) {
-    echo $title . PHP_EOL;
+include('CsdlExtractor.class.php');
+$results = extractFromDir("/root/public_html/CsdlScraping/files.txt");
+var_dump($results);
+
+/**
+ * @param $filename The name of the file which contains the list of terms.
+ * Each of these terms +.html will be the name of a file in the source directory
+ * (typically "html")
+ */
+function extractFromDir($filename)
+{
+    $files0 = file_get_contents($filename);
+    $files = explode("\n", $files0);
+    $titles = array();
+    foreach ($files as $file) {
+        if ($file != "") {
+            $csdl = new \glluchcom\csdlScraping\Extractor(\trim($file));
+            $t = $csdl->getTitles();
+            $titles[$file] = $t;
+        }
+    }
+
+
+return $titles;
+}
+
+/**
+ * Extracts titles only from single file.
+ */
+function extractOne()
+{
+    $csdl = new \glluchcom\csdlScraping\Extractor("information services");
+
+    $titles = $csdl->getTitles(); //extract
+    echo "Titles: " . PHP_EOL;
+    foreach ($titles as $title) {
+        echo $title . PHP_EOL;
+
+    }
 
 }
